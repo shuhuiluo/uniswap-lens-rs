d75e6f428d97737017cd379956d806809e4f7d61
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2025-05-13 23:27:15 -0600

    chore: bump alloy to 1.0.1 and expand bindings (#27)
    Updated alloy dependency to version 1.0.1 and added new contract bindings to support additional Uniswap functionalities. Updated Cargo.toml to reflect these changes.

--- src/bindings.rs
+++ src/bindings.rs
@@ -4,7 +4,6 @@
 ///
 /// * `module_name` - The name of the module to create
 /// * `contract_name` - The name of the contract to bind
-#[macro_export]
 macro_rules! create_sol_binding {
     ($module_name:ident, $contract_name:ident) => {
         pub mod $module_name {
@@ -24,23 +23,25 @@ macro_rules! create_sol_binding {
 }
 
 // Use the macro to create all the bindings
+create_sol_binding!(ephemeralallpositionsbyowner, EphemeralAllPositionsByOwner);
 create_sol_binding!(
     ephemeralgetpopulatedticksinrange,
     EphemeralGetPopulatedTicksInRange
 );
+create_sol_binding!(ephemeralgetposition, EphemeralGetPosition);
+create_sol_binding!(ephemeralgetpositions, EphemeralGetPositions);
 create_sol_binding!(ephemeralpoolpositions, EphemeralPoolPositions);
 create_sol_binding!(ephemeralpoolslots, EphemeralPoolSlots);
 create_sol_binding!(ephemeralpooltickbitmap, EphemeralPoolTickBitmap);
 create_sol_binding!(ephemeralpoolticks, EphemeralPoolTicks);
-create_sol_binding!(ephemeralallpositionsbyowner, EphemeralAllPositionsByOwner);
-create_sol_binding!(ephemeralgetposition, EphemeralGetPosition);
-create_sol_binding!(ephemeralgetpositions, EphemeralGetPositions);
 create_sol_binding!(ephemeralstoragelens, EphemeralStorageLens);
 
-#[cfg(test)]
 create_sol_binding!(iuniswapv3pool, IUniswapV3Pool);
-#[cfg(test)]
 create_sol_binding!(
     iuniswapv3nonfungiblepositionmanager,
     IUniswapV3NonfungiblePositionManager
 );
+
+create_sol_binding!(ierc20, IERC20);
+create_sol_binding!(ierc20metadata, IERC20Metadata);
+create_sol_binding!(ierc721enumerable, IERC721Enumerable);
